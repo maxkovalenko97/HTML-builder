3484c25c25f2841a6715304724e159352327e29c
Max Kovalenko <tutmrmaxi@gmail.com>
2022-05-23 21:54:16 +0300

    fix: add promises to 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,22 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
-const pathToStyles = path.join(__dirname,'styles');
+createCssFile() 
 
-fs.readdir(pathToStyles,{ withFileTypes: true }, (err, files) => {
-    if(err) console.log(err);
-
-    else {
-        files.forEach(file => {
-            path.extname(file.name) === '.css' ? addToBundle(file.name) : true;
-        });
-    }
-})
-
-function addToBundle(file){
+async function createCssFile() {
+    const pathToStyles = path.join(__dirname,'styles');
+    const filesOfStyles = await fs.readdir(pathToStyles, {withFileTypes: true});
     const bundle = path.join(__dirname,'project-dist','bundle.css');
-    const currentFile = path.join(__dirname, 'styles', file);
+    
+    for(let file of filesOfStyles) {
+        if(path.extname(file.name) === '.css') {
+            await addToBundle(file.name);
+        } 
+    }
 
-    fs.readFile(currentFile, 'utf8', function(err, data){
-        fs.appendFile(bundle, data, (err) => {
-            if (err) throw err;
-        })
-    });
+    async function addToBundle (file) {
+        const currentFile = path.join(__dirname, 'styles', file);
+        const dataFile = await fs.readFile(currentFile, 'utf-8');
+        await fs.appendFile(bundle, dataFile);
+    }
 }
