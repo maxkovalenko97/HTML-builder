7e4dbf03d6a68ea2d94f96567e35c70ba7faafca
Max Kovalenko <tutmrmaxi@gmail.com>
2022-05-23 23:01:21 +0300

    fix: correct update-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,15 +1,27 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
-  if (err) throw err;
-});
 
-fs.readdir(path.join(__dirname, 'files'), (err,files) => {
-  if (err) throw err;
-  files.forEach(file => {        
-    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
-      if (err) throw err;
-    });
-  });
-});
+copyDirectory(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
+
+async function copyDirectory(from, to) {
+  await fs.rm(to, { recursive: true, force: true});
+  await fs.mkdir(to, {recursive: true});
+  const copyFrom = from;
+  const copyTo = to;
+
+  deepCopy(copyFrom, copyTo);
+  async function deepCopy(from, to){
+    const contentDir = await fs.readdir(from);
+
+    for(let obj of contentDir) {
+      let stat1 = await fs.stat(path.join(from, obj));
+      if (stat1.isDirectory()) {
+        await fs.mkdir(to + '/' + obj, {recursive: true});
+        await deepCopy(from + '/' + obj, to + '/' + obj); ///////////////
+      } else {
+        await fs.copyFile(path.join(from, obj), path.join(to, obj));
+      }
+    }
+  }
+}
